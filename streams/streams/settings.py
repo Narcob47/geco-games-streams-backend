432558d8706b738f6a872a6c713658e4aa295b48
Bulemu Shilukukwa <narcoa47@gmail.com>
2025-01-29 16:35:36 +0200

    settings

--- streams/streams/settings.py
+++ streams/streams/settings.py
@@ -53,7 +53,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://192.168.1.122:8081',
-    # 'https://geco-games-streams-backend.onrender.com'
+    'https://geco-games-streams-backend.onrender.com'
 ]
 
 REST_FRAMEWORK = {
